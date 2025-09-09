dd989a8a7983ba8598ba517cbf40b26dababfb6e
amanvarshney07 <varshneyaman33@gmail.com>
2025-09-09 18:23:19 +0530

    [Aman Varshney]Added Exception Handling Problem 3

---
+++ AgeValidation.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// Custom Exception
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class AgeValidation {
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        } else {
+            System.out.println("Access granted!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = scanner.nextInt();
+            validateAge(age);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
