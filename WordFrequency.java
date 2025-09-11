f345efa9b67e82520eaac53788ad0b0a45ff73b4
amanvarshney07 <varshneyaman33@gmail.com>
2025-09-11 23:02:32 +0530

    [Aman Varshney] added Collectors Problem 3

--- WordFrequency.java
+++ WordFrequency.java
@@ -1,3 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.function.Function;
+
 public class WordFrequency {
-    
+    public static void main(String[] args) {
+        String paragraph = "The quick brown fox jumps over the lazy dog. The dog was sleeping under the tree.";
+        
+        Arrays.stream(paragraph.toLowerCase().replaceAll("[^a-zA-Z\\s]", "").split("\\s+"))
+            .collect(Collectors.toMap(Function.identity(), w -> 1, Integer::sum))
+            .forEach((word, count) -> System.out.println(word + ": " + count));
+    }
 }
